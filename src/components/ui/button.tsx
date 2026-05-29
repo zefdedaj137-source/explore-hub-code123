33b0aa36b489bfb3c1f72a761e828f3a324ce4f7
zefdedaj137-source <zefdedaj137@gmail.com>
2026-05-29 21:33:35 +0200

    fix: increase button touch targets to 44px minimum for iOS/Android

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -19,9 +19,9 @@ const buttonVariants = cva(
       },
       size: {
         default: "h-11 px-6 py-2",
-        sm: "h-9 px-4",
+        sm: "h-10 px-4",
         lg: "h-12 px-8 text-base",
-        icon: "h-10 w-10",
+        icon: "h-11 w-11",
       },
     },
     defaultVariants: {
@@ -32,8 +32,7 @@ const buttonVariants = cva(
 );
 
 export interface ButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof buttonVariants> {
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>, VariantProps<typeof buttonVariants> {
   asChild?: boolean;
 }
 
