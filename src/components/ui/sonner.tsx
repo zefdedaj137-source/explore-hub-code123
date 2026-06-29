191034511fa2860351126d0e9f36e5591dc9763c
zefdedaj137-source <zefdedaj137@gmail.com>
2026-06-29 20:29:17 +0200

    fix: iOS momentum scrolling + shorten toast duration to 1.8s

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -10,6 +10,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
     <Sonner
       theme={theme as ToasterProps["theme"]}
       className="toaster group"
+      duration={1800}
       toastOptions={{
         classNames: {
           toast:
